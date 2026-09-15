15c6120bacc1d504a76cb64da67df27969b6b2b9
receiver-dummy <teidesat@ull.edu.es>
2026-09-15 19:09:01 +0100

    disable headers to test message encoding

--- transmitter/experiment_buffer.py
+++ transmitter/experiment_buffer.py
@@ -31,7 +31,9 @@ class ExperimentBuffer:
   def insert(self, data):
     TEIDESAT_BIN = "0101010001000101010010010100010001000101010100110100000101010100"
     TASEDIET_BIN = "0101010001000001010100110100010101000100010010010100010101010100"
-    message = TEIDESAT_BIN + data["message"] + TASEDIET_BIN
+    # message = TEIDESAT_BIN + data["message"] + TASEDIET_BIN
+    message = data["message"]
+
     settings = data["settings"]
     experiment_id = data["experiment_id"]
     stripped_experiment_id = experiment_id[:experiment_id.find("M")]
